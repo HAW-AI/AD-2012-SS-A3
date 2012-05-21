2ccd2c775ba5f0cfe155e13488c3ab95121ee5f5
Darius Murawski <dariusgmurawski@goooglemail.com>
2012-05-21 20:48:29 +0200

    ReadDiGraph umgebaut das er die Externen Daten lesen kann. Noch nicht schön aber es funktioniert...

--- a3/IOManager.java
+++ a3/IOManager.java
@@ -5,6 +5,8 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * IOManager zum Laden von Graphen aus Dateien und zur Ausgabe von Logs.
@@ -23,36 +25,62 @@ public class IOManager {
 	public IOManager(String filePath) {
 		this.filePath = filePath;
 		Timestamp tstamp = new Timestamp(System.currentTimeMillis());
-		logFile = new File("SimResult_" + tstamp.toString().replace(":", "-") + ".txt");
+		logFile = new File("SimResult_" + tstamp.toString().replace(":", "-")
+				+ ".txt");
 	}
 
 	/**
 	 * Liest einen Matrix ein
 	 * 
-	 * @return Es wird ein quadratisches, symmetrisches int[][] zurückgegeben <br>
+	 * @return Es wird ein quadratisches, symmetrisches int[][] zurï¿½ckgegeben <br>
 	 *         falls ein Fehler auftritt, wird ein normalisierter Graph der
-	 *         Größe 3 zurückgegeben
+	 *         Grï¿½ï¿½e 3 zurï¿½ckgegeben
 	 */
 	public int[][] readMatrix() {
 
 		int[][] failMatrix = { { 0, 1, 2 }, { 1, 0, 1 }, { 2, 1, 0 } };
 		try {
-			BufferedReader input = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
+			BufferedReader input = new BufferedReader(new InputStreamReader(
+					new FileInputStream(filePath)));
 			try {
 				String[] xAry = input.readLine().split(" 0 | 0|^0 ");
 
-				int[][] matrix = new int[xAry.length][xAry.length];
-				String[] elem;
+				// normale Graphen
+				if (xAry.length != 0) {
+					int[][] matrix = new int[xAry.length][xAry.length];
+					String[] elem;
 
-				for (int i = 1; i < matrix.length; i += 1) {
-					elem = xAry[i].split(" ");
-					for (int j = 0; j < elem.length; j += 1) {
-						matrix[i][j] = Integer.parseInt(elem[j]);
-						matrix[j][i] = matrix[i][j];
+					for (int i = 1; i < matrix.length; i += 1) {
+						elem = xAry[i].split(" ");
+						for (int j = 0; j < elem.length; j += 1) {
+							matrix[i][j] = Integer.parseInt(elem[j]);
+							matrix[j][i] = matrix[i][j];
+						}
 					}
+					input.close();
+					return matrix;
+				} else // DiGraphen
+				{
+					xAry = input.readLine().split(" ");
+					int size = Integer.parseInt(xAry[0]);
+					int[][] matrix = new int[size][size];
+					for (int i = 1; i < matrix.length; i += 1) {
+						String[] elem = xAry[i].split(" ");
+						for (int j = 0; j < elem.length; j += 1) {
+							int tmp = Integer.parseInt(elem[j]);
+							if (tmp % 9 == 0)
+								matrix[i][j] = -1;
+							else {
+								matrix[i][j] = tmp;
+							}
+
+						}
+					}
+					input.close();
+					System.out.print(matrix);
+					return matrix;
 				}
-				input.close();
-				return matrix;
+
 			} catch (IOException e) {
 				System.out.println("IOException: " + e.getMessage());
 				return failMatrix;
@@ -63,16 +91,17 @@ public class IOManager {
 		}
 	}
 
-	
 	public int readBestSolution() {
 		try {
-			BufferedReader input = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
+			BufferedReader input = new BufferedReader(new InputStreamReader(
+					new FileInputStream(filePath)));
 			try {
 				String elem = input.readLine();
 				while (elem != null) {
 					if (elem.startsWith("BEST_SOLUTION")) {
 						input.close();
-						return Integer.parseInt(elem.split("BEST_SOLUTION ")[1]);
+						return Integer
+								.parseInt(elem.split("BEST_SOLUTION ")[1]);
 					}
 					elem = input.readLine();
 				}
@@ -124,21 +153,24 @@ public class IOManager {
 	public void logToConsole(String logString) {
 		System.out.println(logString);
 	}
-        
+
 	/**
-	 * Erzeugt eine vollständige oder unvollsändige unsymetrische zufällige Distanzmatrix.
+	 * Erzeugt eine vollstï¿½ndige oder unvollsï¿½ndige unsymetrische zufï¿½llige
+	 * Distanzmatrix.
+	 * 
 	 * @preCondition maxdistance >=1
 	 * @param seed
-	 *          Der Seed der für den Aufruf von Random benutzt werden soll
-     * @param maxdistance
-     *          Maximale Entfernung zwischen den Kanten
-     * @param size
-     *          Anzahl der gewünschten Zeilen und Spalten
-     * @param chanceofcorrupted
-     *          Wahrscheinlichkeit mit der Kanten im Graphen mit -1 belegt werden sollen
-     *          Bei 0, entsteht ein vollstÃ¤ndiger unsymetrischer Graph
-     *          Bei >0, entsteht ein unvollstÃ¤ndiger unsymetrischer Graph
-	 */        
+	 *            Der Seed der fï¿½r den Aufruf von Random benutzt werden soll
+	 * @param maxdistance
+	 *            Maximale Entfernung zwischen den Kanten
+	 * @param size
+	 *            Anzahl der gewï¿½nschten Zeilen und Spalten
+	 * @param chanceofcorrupted
+	 *            Wahrscheinlichkeit mit der Kanten im Graphen mit -1 belegt
+	 *            werden sollen Bei 0, entsteht ein vollstÃ¤ndiger unsymetrischer
+	 *            Graph Bei 1>x>0, entsteht ein unvollstÃ¤ndiger unsymetrischer
+	 *            Graph
+	 */
 	public static int[][] RandomDistanceMatrix(int seed, int maxdistance,
 			int size, double chanceofcorrupted) {
 		Random rand = new Random(seed);
@@ -167,63 +199,74 @@ public class IOManager {
 		}
 		return result;
 	}
-        
+
 	/**
-     * Liest eine Matrix als Digraph ein
-     *
-     * @return Es wird ein quadratisches int[][] zurückgegeben
-     * <br> falls ein Fehler auftritt, wird ein normalisierter Graph der Größe 3
-     * zurückgegeben
-     */
-    public int[][] readDigraphMatrix() {
+	 * Liest eine Matrix als Digraph ein
+	 * 
+	 * @return Es wird ein quadratisches int[][] zurï¿½ckgegeben <br>
+	 *         falls ein Fehler auftritt, wird ein normalisierter Graph der
+	 *         Grï¿½ï¿½e 3 zurï¿½ckgegeben
+	 */
+	public int[][] readDigraphMatrix() {
 
-        int[][] failMatrix = {{0, 1, 2}, {1, 0, 1}, {2, 1, 0}};
-        try {
-            BufferedReader input = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)));
-            try {
-                String s;
-                // -- Folgender Codeblock sucht den Anfang der EDGE_WEIGHT_SECTION
-                do {
-                    s = input.readLine();
-                } while (s != null && !s.contains("EDGE_WEIGHT_SECTION"));
+		String dimension = "DIMENSION:";
+		int[][] failMatrix = { { 0, 1, 2 }, { 1, 0, 1 }, { 2, 1, 0 } };
+		try {
+			BufferedReader input = new BufferedReader(new InputStreamReader(
+					new FileInputStream(
+							"C:/Users/Car/Downloads/ALL_atsp/p43.atsp")));
+			try {
+				// Lese Datei komplett ein.
+				StringBuilder sb = new StringBuilder();
+				StringBuilder sb2 = new StringBuilder();  // Kopie erzeugen, da Pattern den String zerstÃ¶rt
+				
+				String s;
 
-                // -- Folgender Codeblock sucht das Ende der EDGE_WEIGHT_SECTION
-                // -- Markierung ist EOF
-                // -- und speicert alle Zeilen mit Informationen zu Kantenlängen
-                // -- in einer Liste
-                List<String> l = new ArrayList<String>();
-                StringBuilder sb = new StringBuilder();
-                s = input.readLine();
-                while (s != null && !s.trim().equals("EOF")) {
-                    sb.append(s);
-                    int index = sb.indexOf("  ");
-                    while (index >= 0) {
-                        sb.deleteCharAt(index);
-                        index = sb.indexOf("  ");
-                    }
-                    l.add(sb.toString());
-                    sb.delete(0, sb.length());
-                    s = input.readLine();
-                }
+				do {
+					s = input.readLine();
+					sb.append(s);
+					sb2.append(s);
+				} while (s != null && !s.contains("EOF"));
+				Pattern pattern = Pattern.compile(dimension + "\\s*[\\d]*");
+				Matcher matcher = pattern.matcher(sb);
+				int size = 0;
+				while (matcher.find())
+					size = Integer.parseInt(matcher.group().split(" ")[1]
+							.trim());
 
-                int[][] matrix = new int[l.size()][l.size()];
-                String[] elem;
+				int[][] matrix = new int[size][size];
+				int index = sb2.indexOf(dimension);
+				String f = sb2.substring(index + dimension.length()
+						+ Integer.toString(size).length() + 1);
 
-                for (int i = 0; i < matrix.length; i++) {
-                    elem = l.get(i).split(" ");
-                    for (int j = 0; j < elem.length; j++) {
-                        matrix[i][j] = Integer.parseInt(elem[j]);
-                    }
-                }
-                input.close();
-                return matrix;
-            } catch (IOException e) {
-                System.out.println("IOException: " + e.getMessage());
-                return failMatrix;
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found!");
-            return failMatrix;
-        }
-    }
+				Pattern pattern2 = Pattern.compile("\\s*[\\d]*\\s*\\t*");
+				Matcher matcher2 = pattern2.matcher(f);
+				int i = 0;
+				while (matcher2.find()) {
+					String tempzahl = matcher2.group().trim();
+					if (!tempzahl.isEmpty()) {
+						// Falls nicht Hauptdiagonale, bei 0 mit -1 ersetzen
+						int buff = Integer.parseInt(tempzahl);
+						if ((i / size) ==( i % size) && buff==0) {
+							buff = -1;
+						}
+						
+						matrix[i / size][i % size] = buff;
+						i++;
+					}
+
+				}
+
+				input.close();
+				return matrix;
+			} catch (IOException e) {
+				System.out.println("IOException: " + e.getMessage());
+				return failMatrix;
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found!");
+			return failMatrix;
+		}
+
+	}
 }
