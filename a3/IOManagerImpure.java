10f603cadde4011564f5e80813b1f5819ba8d83c
Bjoern Eberhardt <lazer.erazer@gmail.com>
2012-06-06 12:47:22 +0200

    Neuer IOManager

---
+++ a3/IOManagerImpure.java
@@ -0,0 +1,162 @@
+package a3;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * IOManager zum Laden von Graphen aus Dateien und zur Ausgabe von Logs.
+ */
+public class IOManagerImpure {
+
+    private final String filePath;
+    private int[] failMatrix = {0, 1, 2, 1, 0, 1, 1, 2, 0};
+
+    /**
+     * Erstellt den IOManager zur Ausgabe ins Logfile und auf die Konsole <br>
+     * Erzeugt die Ausgabedatei
+     * 
+     * @param filePath
+     *            der Pfad zur Eingabedatei relativ zum src-Verzeichnis
+     */
+    public IOManagerImpure(String filePath) {
+        this.filePath = filePath;
+    }
+
+    /**********************************************
+     * 
+     *      getEdgeWeightSection()
+     *      getDemandSection()
+     *      getDimension()
+     *      getAntCapacity()
+     * 
+     **********************************************/
+    public int[] getEdgeWeightSection() {
+        int[] edgeDistance = null;
+        int[][] testAry = null;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(new File(filePath)))) {
+            String line = br.readLine();
+
+            while (!(line = br.readLine()).equals("EOF")) {
+                if (line.equals("EDGE_WEIGHT_SECTION")) {
+                    edgeDistance = new int[getDimension() * getDimension()];
+                    testAry = new int[getDimension()][getDimension()];
+                    int index = 0;
+                    int j = 0;
+                    while (!(line = br.readLine()).equals("DEMAND_SECTION") && !line.equals("EOF")) {
+                        String unspaced = line.trim();
+                        String[] splited = unspaced.split("(\\s)+");
+                        for (int i = 0; i < splited.length; i++) {
+                            edgeDistance[index] = Integer.parseInt(splited[i].trim());
+                            index++;
+                            testAry[j][i] = Integer.parseInt(splited[i].trim());
+                        }
+                        j++;
+                    }
+                    break;
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.println("Fehler beim Lesen der Datei!");
+        }
+        //Der Weg von Ecke 1 nach Ecke 2 endet in einer Sackgasse => sollte nicht geladen werden
+
+        int counter = 0; //um -1's zu zahlen
+        for (int i = 0; i < testAry.length; i++) {
+            for (int j = 0; j < testAry.length; j++) {
+                if (testAry[i][j] == -1) {
+                    counter++;
+                }
+            }
+            if (counter == testAry.length - 1) {
+                fehlerausgabe("Eine Sackgasse oder nichtzusammenhaendender Graph.");
+                System.exit(0);
+            }
+            counter = 0;
+        }
+        return edgeDistance;
+    }
+
+    public int[] getDemandSection() {
+        int[] edgeCapacity = null;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(new File(filePath)))) {
+            String line = br.readLine();
+
+            while (!(line = br.readLine()).equals("EOF")) {
+
+                if (line.equals("DEMAND_SECTION")) {
+                    edgeCapacity = new int[getDimension()];
+                    int index = 0;
+                    while (!(line = br.readLine()).equals("EOF")) {
+                        String[] splited = line.split("(\\s)+");
+                        edgeCapacity[index] = Integer.parseInt(splited[1].trim());
+                        index++;
+                    }
+                    break;
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.println("Fehler beim Lesen der Datei!");
+        }
+        //Kapazitaet von Ecke ist groesser als die Ameisenkapazitaet => sollte nicht geladen werden
+        int capacity = getAntCapacity();
+        for (int customer : edgeCapacity) {
+            if (customer > capacity) {
+                fehlerausgabe("Die Kapazitaet des LKWs ist kleiner als die groesste Kapazitaet der Kunden.");
+                System.exit(0);
+            }
+        }
+        return edgeCapacity;
+    }
+
+    public int getDimension() {
+        int dimension = 0;
+        try (BufferedReader br = new BufferedReader(new FileReader(new File(filePath)))) {
+            String line = br.readLine();
+            while (!(line = br.readLine()).equals("EOF")) {
+
+                String[] splited = line.split(":");
+                splited[0] = splited[0].trim();
+
+                if (splited[0].equals("DIMENSION")) {
+                    dimension = Integer.parseInt(splited[1].trim());
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.println("Fehler beim Lesen der Datei!");
+        }
+        return dimension;
+    }
+
+    public int getAntCapacity() {
+        int ant = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(new File(filePath)))) {
+            String line = br.readLine();
+
+            while (!(line = br.readLine()).equals("EOF")) {
+                String[] splited = line.split(":");
+                splited[0] = splited[0].trim();
+
+                if (splited[0].equals("CAPACITY")) {
+                    ant = Integer.parseInt(splited[1].trim());
+                    break;
+                }
+            }
+            br.close();
+        } catch (IOException e) {
+            System.out.println("Fehler beim Lesen der Datei!");
+        }
+        return ant;
+    }
+
+    private void fehlerausgabe(String text) {
+        System.out.println("Fehler beim Einlesen der Datei aus : " + filePath + "   " + text);
+    }
+}
